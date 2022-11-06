14a158607e47e9b7721df34715943d540f2bcdc3
Arrowin24 <igor_rudenko.ru@mail.ru>
2022-11-06 23:40:14 +0700

    Решение ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
 
+        try {
+            Authorization.addNewProfile("refsdr", "123", "123");
+        } catch (WrongLoginExeption e) {
+            System.out.println("Ошибка логина");
+        } catch (WrongPasswordException e) {
+            System.out.println("Ошибка пароля: " + e.getMessage());
+        }
     }
 }
